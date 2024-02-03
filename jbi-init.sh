2cb2aa3f16f8d77b352ba4cb4a0a725947573f33
MichaelOC23 <135385263+MichaelOC23@users.noreply.github.com>
2024-02-03 11:41:07 -0800

    Update jbi-init.sh

--- jbi-init.sh
+++ jbi-init.sh
@@ -7,17 +7,13 @@ mkdir -p $JBI_FOLDER
 destination_folder="/path/to/destination/folder"
 file_name="jbi-setup.sh"
 
-url="https://raw.githubusercontent.com/user/repo/branch/path/to/file.sh"
+url="https://github.com/MichaelOC23/setup/tree/main/jbi-setup.sh"
 
 # Define the destination folder
 destination_folder="/path/to/destination/folder"
 
 # Use curl to download the file
-curl -L $url -o $destination_folder/file.sh
-
-
-# Run the script with bash
-bash $destination_folder/$file_name
+curl -L $url -o "$destination_folder/$file_name"
 
 # Or make it executable and then run it
 chmod +x $destination_folder/$file_name
