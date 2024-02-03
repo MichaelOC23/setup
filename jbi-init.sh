ed72c06fe5af7ce3f531dc2a3175c0609f65b71e
MichaelOC23 <135385263+MichaelOC23@users.noreply.github.com>
2024-02-03 13:03:28 -0800

    Update jbi-init.sh

--- jbi-init.sh
+++ jbi-init.sh
@@ -6,17 +6,28 @@
 cd ~
 JBI_FOLDER = ".jbi"
 
+echo $JBI_FOLDER
+mkdir -p $JBI_FOLDER
+
 JBI_FOLDER_PATH="$HOME/$JBI_FOLDER"
+
+echo $JBI_FOLDER_PATH
 
 setup_file_name="jbi-setup.sh"
 env_file_name="env_variables.sh"
+
+setup_file_path = "$JBI_FOLDER_PATH/$setup_file_name"
+env_file_path = "$JBI_FOLDER_PATH/$env_file_name"
+
+echo $setup_file_path
+echo $env_file_path
 
 setup_url="https://raw.githubusercontent.com/MichaelOC23/setup/main/jbi-setup.sh"
 env_url="https://raw.githubusercontent.com/MichaelOC23/setup/main/env_variables.sh"
 
 # Use curl to download the file
-curl -L $setup_url -o "$JBI_FOLDER_PATH/$setup_file_name"
-curl -L $env_url -o "$JBI_FOLDER_PATH/$env_file_name"
+curl -L $setup_url -o $setup_file_path
+curl -L $env_url -o $env_file_path
 
 # Or make it executable and then run it
 chmod +x $JBI_FOLDER_PATH/$setup_file_name
