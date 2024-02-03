3966f7b5aac3354a3185719fbc0661e6fc657b2f
MichaelOC23 <135385263+MichaelOC23@users.noreply.github.com>
2024-02-03 12:31:12 -0800

    Update jbi-init.sh

--- jbi-init.sh
+++ jbi-init.sh
@@ -1,28 +1,30 @@
-#!bin/bash 
+#!/bin/bash 
 #Install Command
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/MichaelOC23/setup/main/jbi-init.sh)"
 
 #Custom hidden root folder for JBI machines to install software
+cd ~
+JBI_FOLDER = ".jbi"
 
-JBI_FOLDER = "$HOME/.jbi"
-mkdir -p $JBI_FOLDER
+JBI_FOLDER_PATH="$HOME/$JBI_FOLDER"
 
 setup_file_name="jbi-setup.sh"
 env_file_name="env_variables.sh"
 
-setup_url="https://github.com/MichaelOC23/setup/tree/main/jbi-setup.sh"
-setup_url="https://github.com/MichaelOC23/setup/tree/main/env_variables.sh"
+ssetup_url="https://raw.githubusercontent.com/MichaelOC23/setup/main/jbi-setup.sh"
+setup_url="https://raw.githubusercontent.com/MichaelOC23/setup/main/env_variables.sh"
 
 # Use curl to download the file
-curl -L $url -o "$JBI_FOLDER/$setup_file_name"
-curl -L $url -o "$JBI_FOLDER/$env_file_name"
+curl -L $url -o "$JBI_FOLDER_PATH/$setup_file_name"
+curl -L $url -o "$JBI_FOLDER_PATH/$env_file_name"
 
 # Or make it executable and then run it
-chmod +x $JBI_FOLDER/$setup_file_name
-chmod +x $JBI_FOLDER/$env_file_name
+chmod +x $JBI_FOLDER_PATH/$setup_file_name
+chmod +x $JBI_FOLDER_PATH/$env_file_name
 
-echo "export PATH=\"\$PATH:\$HOME/.jbi/env_variables.sh"\" >> ~/test
-
-$JBI_FOLDER/$setup_file_name
-echo 
+echo "export PATH=\"\$PATH:\$HOME/.jbi/env_variables.sh"\" >> ~/.zshrc
 source "$HOME/.zshrc"
+
+$JBI_FOLDER_PATH/$setup_file_name
+
+
