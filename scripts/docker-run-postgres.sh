7378900b1fa98236545a31236927bfbcfbb41943
MichaelOC23 <135385263+MichaelOC23@users.noreply.github.com>
2024-05-24 19:52:03 -0700

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   .DS_Store
    	modified:   .gitignore
    	deleted:    scripts/deep_m4a_to_mp3_folder.sh
    	modified:   scripts/docker-run-postgres.sh

--- scripts/docker-run-postgres.sh
+++ scripts/docker-run-postgres.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
+LOCAL_DATA_PATH="/Users/michasmi/code/mytech/postgresql"
+LOCAL_PORT="5400:5432"
+
+rm -rf "${LOCAL_DATA_PATH}"
+
 docker run \
-  --hostname=f258662f79d8 \
-  --user=1001 \
-  --env=PATH=/opt/bitnami/postgresql/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-  --env=HOME=/ \
-  --env=OS_ARCH=arm64 \
-  --env=OS_FLAVOUR=debian-12 \
-  --env=OS_NAME=linux \
-  --env=APP_VERSION=16.3.0 \
-  --env=BITNAMI_APP_NAME=postgresql \
-  --env=ALLOW_EMPTY_PASSWORD=yes \
-  --env=LANG=en_US.UTF-8 \
-  --env=LANGUAGE=en_US:en \
-  --env=NSS_WRAPPER_LIB=/opt/bitnami/common/lib/libnss_wrapper.so \
-  --volume=/bitnami/postgresql \
-  --volume=/docker-entrypoint-initdb.d \
-  --volume=/docker-entrypoint-preinitdb.d \
+  --name=mytech-postgresql \
+  --hostname=mytech-postgresql-host \
+  --env=POSTGRES_DB=mytech \
+  --env=POSTGRES_PASSWORD=mytech \
+  --env=POSTGRES_USER=mytech \
+  --env=PGDATA=/var/lib/postgresql/data \
+  --volume="${LOCAL_DATA_PATH}:/var/lib/postgresql/data" \
+  --env=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+  --env=LANG=en_US.utf8 \
+  --network=platform_default -p "${LOCAL_PORT}" \
   --restart=no \
-  --label='com.vmware.cp.artifact.flavor=sha256:c50c90cfd9d12b445b011e6ad529f1ad3daea45c26d20b00732fae3cd71f6a83' \
-  --label='org.opencontainers.image.base.name=docker.io/bitnami/minideb:bookworm' \
-  --label='org.opencontainers.image.created=2024-05-20T21:42:18Z' \
-  --label='org.opencontainers.image.description=Application packaged by Broadcom, Inc.' \
-  --label='org.opencontainers.image.documentation=https://github.com/bitnami/containers/tree/main/bitnami/postgresql/README.md' \
-  --label='org.opencontainers.image.licenses=Apache-2.0' \
-  --label='org.opencontainers.image.ref.name=16.3.0-debian-12-r8' \
-  --label='org.opencontainers.image.source=https://github.com/bitnami/containers/tree/main/bitnami/postgresql' \
-  --label='org.opencontainers.image.title=postgresql' \
-  --label='org.opencontainers.image.vendor=Broadcom, Inc.' \
-  --label='org.opencontainers.image.version=16.3.0' \
-  --runtime=runc \
-  -d bitnami/postgresql:latest
+  --runtime=runc -d postgres:14-alpine
+
+# Wait for 5 seconds
+sleep 5
+
+# Attempt to connect to the PostgreSQL server
+psql -h localhost -p 4999 -U mytech -d mytech
